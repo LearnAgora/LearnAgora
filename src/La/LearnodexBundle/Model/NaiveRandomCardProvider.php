3383a98101c5dee72880cead63b4c8a71f845e80
Alexander Deruwe <alexander.deruwe@gmail.com>
2014-09-20 17:39:31 +0200

    LN-48: Use the random card provider service, through dependency injection

--- src/La/LearnodexBundle/Model/NaiveRandomCardProvider.php
+++ src/La/LearnodexBundle/Model/NaiveRandomCardProvider.php
@@ -3,11 +3,14 @@
 namespace La\LearnodexBundle\Model;
 
 use Doctrine\Common\Persistence\ObjectRepository;
+use JMS\DiExtraBundle\Annotation as DI;
 use La\LearnodexBundle\Model\Exception\CardNotFoundException;
 
 /**
  * This random card provider naively fetches all the cards from the
  * repository, just to return the one from the whole set.
+ *
+ * @DI\Service("naive.random.card.provider")
  */
 class NaiveRandomCardProvider implements RandomCardProviderInterface
 {
@@ -20,6 +23,10 @@ class NaiveRandomCardProvider implements RandomCardProviderInterface
      * Constructor.
      *
      * @param ObjectRepository $learningEntityRepository
+     *
+     * @DI\InjectParams({
+     *     "learningEntityRepository" = @DI\Inject("la_core.repository.action")
+     * })
      */
     public function __construct(ObjectRepository $learningEntityRepository)
     {
--- src/La/SandboxBundle/Controller/CardController.php
+++ src/La/SandboxBundle/Controller/CardController.php
@@ -3,31 +3,35 @@
 namespace La\SandboxBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
-use La\CoreBundle\Entity\LearningEntity;
-use La\LearnodexBundle\Model\Card;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use JMS\DiExtraBundle\Annotation as DI;
+use La\LearnodexBundle\Model\RandomCardProviderInterface;
 
-class CardController extends Controller
+class CardController
 {
+    /**
+     * @var RandomCardProviderInterface
+     */
+    private $randomCardProvider;
+
+    /**
+     * Constructor.
+     *
+     * @param RandomCardProviderInterface $randomCardProvider
+     *
+     * @DI\InjectParams({
+     *     "randomCardProvider" = @DI\Inject("naive.random.card.provider")
+     * })
+     */
+    public function __construct(RandomCardProviderInterface $randomCardProvider)
+    {
+        $this->randomCardProvider = $randomCardProvider;
+    }
+
     /**
      * @Rest\View
      */
     public function randomAction()
     {
-        $em = $this->getDoctrine()->getManager();
-
-        //whoaa .. for now it works but i should find a way to get a random action
-        $learningEntities = $em->getRepository('LaCoreBundle:Action')->findAll();
-        if (count($learningEntities)) {
-            shuffle($learningEntities);
-
-            /** @var $learningEntity LearningEntity */
-            $learningEntity = $learningEntities[0];
-            $card = new Card($learningEntity);
-
-            return array('card' => $card);
-        }
-
-        return array('test' => 'grrr');
+        return $this->randomCardProvider->get();
     }
 }
