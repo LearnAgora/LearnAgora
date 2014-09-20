b498aa5f73ce36eddb4cda4134701ef967267a51
Alexander Deruwe <alexander.deruwe@gmail.com>
2014-09-20 20:36:55 +0200

    LN-49: Document the random card API endpoint

--- src/La/SandboxBundle/Controller/CardController.php
+++ src/La/SandboxBundle/Controller/CardController.php
@@ -4,7 +4,10 @@ namespace La\SandboxBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 use JMS\DiExtraBundle\Annotation as DI;
+use La\LearnodexBundle\Model\Exception\CardNotFoundException;
 use La\LearnodexBundle\Model\RandomCardProviderInterface;
+use Nelmio\ApiDocBundle\Annotation as Doc;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CardController
 {
@@ -29,9 +32,23 @@ class CardController
 
     /**
      * @Rest\View
+     *
+     * @Doc\ApiDoc(
+     *  section="Learnodex",
+     *  description="Retrieves a random card",
+     *  statusCodes={
+     *      200="Returned when successful",
+     *      404="Returned when no card is found",
+     *  })
      */
     public function randomAction()
     {
-        return $this->randomCardProvider->get();
+        try {
+            $card = $this->randomCardProvider->get();
+        } catch (CardNotFoundException $e) {
+            throw new NotFoundHttpException(null, $e);
+        }
+
+        return $card;
     }
 }
