97e31431fb5595185ed592992aad748e5ef007d6
Alexander Deruwe <alexander.deruwe@gmail.com>
2014-09-23 18:37:53 +0200

    LN-63: MultipleChoiceQuestion, Answer and Action fixtures

--- src/La/CoreBundle/DataFixtures/ORM/LoadActionData.php
+++ src/La/CoreBundle/DataFixtures/ORM/LoadActionData.php
@@ -17,6 +17,7 @@ class LoadActionData extends AbstractFixture implements DependentFixtureInterfac
         return array(
             'La\CoreBundle\DataFixtures\ORM\LoadUrlContentData',
             'La\CoreBundle\DataFixtures\ORM\LoadSimpleQuestionData',
+            'La\CoreBundle\DataFixtures\ORM\LoadMultipleChoiceQuestionData',
             'La\CoreBundle\DataFixtures\ORM\LoadUserData',
         );
     }
@@ -32,6 +33,9 @@ class LoadActionData extends AbstractFixture implements DependentFixtureInterfac
         $action4 = $this->createAction('simple-question-1', 'user-anna', 'Planning processes');
         $action5 = $this->createAction('simple-question-2', 'user-anna', 'Immersive');
         $action6 = $this->createAction('simple-question-3', 'user-anna', 'Change Management');
+        $action7 = $this->createAction('multiple-choice-question-1', 'user-anna', 'Pop quiz on visual perception');
+        $action8 = $this->createAction('multiple-choice-question-2', 'user-anna', 'Design examples');
+        $action9 = $this->createAction('multiple-choice-question-3', 'user-anna', 'Chelsea MVP');
 
         $this->addReference('action-1', $action1);
         $this->addReference('action-2', $action2);
@@ -39,6 +43,9 @@ class LoadActionData extends AbstractFixture implements DependentFixtureInterfac
         $this->addReference('action-4', $action4);
         $this->addReference('action-5', $action5);
         $this->addReference('action-6', $action6);
+        $this->addReference('action-7', $action7);
+        $this->addReference('action-8', $action8);
+        $this->addReference('action-9', $action9);
 
         $manager->persist($action1);
         $manager->persist($action2);
@@ -46,6 +53,9 @@ class LoadActionData extends AbstractFixture implements DependentFixtureInterfac
         $manager->persist($action4);
         $manager->persist($action5);
         $manager->persist($action6);
+        $manager->persist($action7);
+        $manager->persist($action8);
+        $manager->persist($action9);
 
         $manager->flush();
     }
--- src/La/CoreBundle/DataFixtures/ORM/LoadAnswerData.php
+++ src/La/CoreBundle/DataFixtures/ORM/LoadAnswerData.php
@@ -16,6 +16,7 @@ class LoadAnswerData extends AbstractFixture implements DependentFixtureInterfac
     {
         return array(
             'La\CoreBundle\DataFixtures\ORM\LoadSimpleQuestionData',
+            'La\CoreBundle\DataFixtures\ORM\LoadMultipleChoiceQuestionData',
         );
     }
 
@@ -30,6 +31,11 @@ class LoadAnswerData extends AbstractFixture implements DependentFixtureInterfac
         $answer4 = $this->createAnswer('simple-question-2', 'based on objective movement in the image?');
         $answer5 = $this->createAnswer('simple-question-3', 'Settle on a process with clear inputs/outputs and intermediate stages.');
         $answer6 = $this->createAnswer('simple-question-3', 'Talk to the people and understand how they see themselves in this change.');
+        $answer7 = $this->createAnswer('multiple-choice-question-1', 'David Marr is a Gestalt thinker.');
+        $answer8 = $this->createAnswer('multiple-choice-question-1', 'The ecological view is represented by JJ. Gibson.');
+        $answer9 = $this->createAnswer('multiple-choice-question-1', 'The famous drawings of Escher can be related to Gestalt theory.');
+        $answer10 = $this->createAnswer('multiple-choice-question-1', 'Mihai Fagadar is known for his criticism of the static observer.');
+        $answer11 = $this->createAnswer('multiple-choice-question-1', "Foreground extraction as per Fagadar's work is key to developing immersive approaches.");
 
         $this->addReference('answer-1', $answer1);
         $this->addReference('answer-2', $answer2);
@@ -37,6 +43,11 @@ class LoadAnswerData extends AbstractFixture implements DependentFixtureInterfac
         $this->addReference('answer-4', $answer4);
         $this->addReference('answer-5', $answer5);
         $this->addReference('answer-6', $answer6);
+        $this->addReference('answer-7', $answer7);
+        $this->addReference('answer-8', $answer8);
+        $this->addReference('answer-9', $answer9);
+        $this->addReference('answer-10', $answer10);
+        $this->addReference('answer-11', $answer11);
 
         $manager->persist($answer1);
         $manager->persist($answer2);
@@ -44,6 +55,11 @@ class LoadAnswerData extends AbstractFixture implements DependentFixtureInterfac
         $manager->persist($answer4);
         $manager->persist($answer5);
         $manager->persist($answer6);
+        $manager->persist($answer7);
+        $manager->persist($answer8);
+        $manager->persist($answer9);
+        $manager->persist($answer10);
+        $manager->persist($answer11);
 
         $manager->flush();
     }
