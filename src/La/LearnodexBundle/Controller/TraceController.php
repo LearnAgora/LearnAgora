12ab3a03893734f6dc550558d5e7c98bcc538bef
Bart Kindt <bart.kindt@gmail.com>
2014-10-02 10:28:58 +0200

    make the discard and later buttons work, still some work needed to calculate the affinity

---
+++ src/La/LearnodexBundle/Controller/TraceController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace La\LearnodexBundle\Controller;
+
+use La\CoreBundle\Entity\Affinity;
+use La\CoreBundle\Entity\ButtonOutcome;
+use La\CoreBundle\Entity\LearningEntity;
+use La\CoreBundle\Entity\Answer;
+use La\CoreBundle\Entity\Outcome;
+use La\CoreBundle\Entity\Trace;
+use La\CoreBundle\Model\Outcome\ProcessResultVisitor;
+use La\LearnodexBundle\Model\Card;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use La\CoreBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+
+class TraceController extends Controller
+{
+    public function traceAction(Request $request)
+    {
+        /** @var $user User */
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        $answerId = $request->request->get('answer');
+
+        $em = $this->getDoctrine()->getManager();
+
+        /** @var $answer Answer */
+        $answer = $em->getRepository('LaCoreBundle:Answer')->find($answerId);
+        /** @var $outcome Outcome */
+        foreach ($answer->getOutcomes() as $outcome) {
+            $trace = new Trace();
+            $trace->setUser($user);
+            $trace->setOutcome($outcome);
+            $em->persist($trace);
+            $em->flush();
+            foreach ($outcome->getResults() as $result) {
+                $processResultVisitor = new ProcessResultVisitor($user,$em);
+                $result->accept($processResultVisitor);
+            }
+        }
+
+        return $this->redirect($this->generateUrl('card_auto'));
+    }
+
+    public function traceButtonAction($id, $caption)
+    {
+        /** @var $user User */
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        $em = $this->getDoctrine()->getManager();
+        /** @var $learningEntity LearningEntity */
+        $learningEntity = $em->getRepository('LaCoreBundle:LearningEntity')->find($id);
+
+        /** @var $outcome Outcome */
+        foreach ($learningEntity->getOutcomes() as $outcome) {
+            if (is_a($outcome,'La\CoreBundle\Entity\ButtonOutcome') && $outcome->getCaption() == $caption) {
+                $trace = new Trace();
+                $trace->setUser($user);
+                $trace->setOutcome($outcome);
+                $em->persist($trace);
+                $em->flush();
+                foreach ($outcome->getResults() as $result) {
+                    $processResultVisitor = new ProcessResultVisitor($user,$em);
+                    $result->accept($processResultVisitor);
+                }
+            }
+        }
+
+        return $this->redirect($this->generateUrl('card_auto'));
+
+    }
+}
