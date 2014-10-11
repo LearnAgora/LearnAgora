9de99155c208d5ad9d3d1c84668c1b972dc21132
Alexander Deruwe <alexander.deruwe@gmail.com>
2014-10-12 01:06:14 +0200

    LN-80: Initial configuration

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,19 +1,31 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
+set :application, "dev.learnagora"
+set :domain, "54.171.56.1"
+set :deploy_to, "/var/www/#{domain}"
+set :app_path, "app"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
-set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set :repository, "git@bitbucket.org:aderuwe/learnagora.git"
+set :scm, :git
 
+set :user, "ubuntu"
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", "vendor"]
+set :writable_dirs, ["app/cache", "app/logs"]
+set :webserver_user, "www-data"
+set :permission_method, :acl
+set :use_composer, true
+set :composer_options, "--no-scripts --verbose --optimize-autoloader"
+set :dump_assetic_assets, false
 set :model_manager, "doctrine"
-# Or: `propel`
+set :use_sudo, false
+set :clear_controllers, true
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-set  :keep_releases,  3
+role :web, domain # Your HTTP server, Apache/etc
+role :app, domain, :primary => true # This may be the same as your `Web` server
+
+set :keep_releases, 3
 
 # Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
