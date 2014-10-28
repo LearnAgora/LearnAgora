92688a92dd874beefce7e0fe9c683ebbcd84f446
Alexander Deruwe <alexander.deruwe@gmail.com>
2014-10-28 23:27:24 +0100

    Add LaAngularBundle

---
+++ src/La/AngularBundle/Controller/DefaultController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace La\AngularBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+    }
+}
---
+++ src/La/AngularBundle/LaAngularBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace La\AngularBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LaAngularBundle extends Bundle
+{
+}
