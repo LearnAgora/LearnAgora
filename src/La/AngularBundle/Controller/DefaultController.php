88646cf2820bca9af656bb53c4fbb4325bf45c8b
Alexander Deruwe <alexander.deruwe@gmail.com>
2015-01-28 20:22:56 +0100

    Angular bundle default controller as a service

--- src/La/AngularBundle/Controller/DefaultController.php
+++ src/La/AngularBundle/Controller/DefaultController.php
@@ -2,12 +2,39 @@
 
 namespace La\AngularBundle\Controller;
 
+use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    /**
+     * @var EngineInterface
+     */
+    private $twig;
+
+    /**
+     * Constructor.
+     *
+     * @param EngineInterface $twig
+     *
+     * @DI\InjectParams({
+     *  "twig" = @DI\Inject("templating")
+     * })
+     */
+    public function __construct(EngineInterface $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * Renders the initial card UI.
+     *
+     * @return Response
+     */
     public function cardAction()
     {
-        return $this->render('LaAngularBundle:Default:card.html.twig');
+        return $this->twig->renderResponse('LaAngularBundle:Default:card.html.twig');
     }
 }
