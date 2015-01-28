1638cce0df5e3a6cab4dab55ccec94526692ece2
Alexander Deruwe <alexander.deruwe@gmail.com>
2015-01-28 20:30:26 +0100

    DefaultController unit test

--- src/La/AngularBundle/Tests/Controller/DefaultControllerTest.php
+++ src/La/AngularBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,8 +2,35 @@
 
 namespace La\AngularBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use La\AngularBundle\Controller\DefaultController;
+use Prophecy\PhpUnit\ProphecyTestCase;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends ProphecyTestCase
 {
+    /**
+     * @var EngineInterface
+     */
+    private $twig;
+
+    /**
+     * @var DefaultController
+     */
+    private $sut;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->twig = $this->prophesize('\Symfony\Bundle\FrameworkBundle\Templating\EngineInterface');
+        $this->sut = new DefaultController($this->twig->reveal());
+    }
+
+    /** @test */
+    public function it_only_loads_the_initial_ui()
+    {
+        $this->twig->renderResponse('LaAngularBundle:Default:card.html.twig')->shouldBeCalled()->willReturn('initial ui');
+
+        $this->assertEquals('initial ui', $this->sut->cardAction());
+    }
 }
