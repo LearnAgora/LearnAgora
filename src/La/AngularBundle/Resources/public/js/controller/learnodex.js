a2a04699660acafb658ff86945c3ddd8ffef8d29
Alexander Deruwe <alexander.deruwe@gmail.com>
2015-01-28 22:27:15 +0100

    Slightly better architecture

--- src/La/AngularBundle/Resources/public/js/controller/learnodex.js
+++ src/La/AngularBundle/Resources/public/js/controller/learnodex.js
@@ -1,12 +1,31 @@
 var learnodex = angular.module('learnodex', []);
 
-learnodex.controller('LearnodexController', ['$scope', '$http', function ($scope, $http) {
+learnodex.service('environment', function () {
+    return {
+        isDebug: true
+    };
+});
+
+learnodex.service('api', ['$http', function ($http) {
+    var randomCard = function (data) {
+        $http({method: 'GET', url: '/app_dev.php/api/ln/cards/random'}).success(data.success);
+    };
+
+    return {
+        randomCard: randomCard
+    };
+}]);
+
+learnodex.controller('LearnodexController', ['$scope', 'api', 'environment', function ($scope, api, environment) {
     $scope.newCard = function () {
-        $http({method: 'GET', url: '/app_dev.php/api/ln/cards/random'})
-            .success(function (data) {
-                console.log(data);
+        api.randomCard({
+            success: function (data) {
+                if (environment.isDebug) {
+                    console.log(data);
+                }
+
                 $scope.card = data;
             }
-        );
+        });
     };
 }]);
