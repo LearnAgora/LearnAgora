b0bae4fa1db21e07e7d9fcec64b469fc5933b3bf
Bart Kindt <bart.kindt@gmail.com>
2015-03-12 11:34:28 +0100

    LN-274 : define doctrine entities for probabilities

---
+++ src/La/CoreBundle/Entity/Profile.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace La\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Profile
+ */
+class Profile
+{
+    /**
+     * @var integer
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Profile
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
