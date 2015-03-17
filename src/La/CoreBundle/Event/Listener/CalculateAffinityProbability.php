76c7b4ab15fa652f6941d3e54c6896185703c232
Bart Kindt <bart.kindt@gmail.com>
2015-03-17 08:44:01 +0100

    LN-277 :main algorithm implemented

--- src/La/CoreBundle/Event/Listener/CalculateAffinityProbability.php
+++ src/La/CoreBundle/Event/Listener/CalculateAffinityProbability.php
@@ -2,9 +2,16 @@
 
 namespace La\CoreBundle\Event\Listener;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Persistence\ObjectRepository;
 use JMS\DiExtraBundle\Annotation as DI;
+use La\CoreBundle\Entity\Affinity;
+use La\CoreBundle\Entity\ProbabilityGivenProfile;
+use La\CoreBundle\Entity\Trace;
+use La\CoreBundle\Entity\User;
+use La\CoreBundle\Entity\UserProbability;
 use La\CoreBundle\Event\TraceEvent;
+use La\CoreBundle\Events;
 
 /**
  * @DI\Service
@@ -18,17 +25,25 @@ class CalculateAffinityProbability
     private $userProbabilityRepository;
 
     /**
+     * @var ObjectManager
+     */
+    private $entityManager;
+
+    /**
      * Constructor.
      *
      * @param ObjectRepository $userProbabilityRepository
+     * @param ObjectManager $entityManager
 
      *
      * @DI\InjectParams({
-     *  "userProbabilityRepository" = @DI\Inject("la_core.repository.userProbability")
+     *  "userProbabilityRepository" = @DI\Inject("la_core.repository.userProbability"),
+     *  "entityManager" = @DI\Inject("doctrine.orm.entity_manager")
      * })
      */
-    public function __construct(ObjectRepository $userProbabilityRepository) {
+    public function __construct(ObjectRepository $userProbabilityRepository, ObjectManager $entityManager) {
         $this->userProbabilityRepository = $userProbabilityRepository;
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -38,16 +53,64 @@ class CalculateAffinityProbability
      */
     public function onResult(TraceEvent $traceEvent)
     {
-        //        $this->compareWithPersona($user);
-        /* Trace $trace */
+        /* @var Trace $trace */
         $trace = $traceEvent->getTrace();
+        /* @var User $user */
+        $user = $trace->getUser();
+
         $outcome = $trace->getOutcome();
         $learningEntity = $outcome->getLearningEntity();
+        $parents = $learningEntity->getUplinks();
+        $agora = $parents[0]->getParent();
 
         //load probabilities for this user
-        $userProbabilities = $this->userProbabilityRepository->findFor($trace->getUser(), $learningEntity);
+        $userProbabilities = $this->userProbabilityRepository->findFor($user, $agora);
 
         $outcomeProbabilities = $outcome->getProbabilities();
 
+        $num = count($userProbabilities);
+        if ($num == 0) {
+            foreach ($outcomeProbabilities as $outcomeProbability) {
+                /* @var ProbabilityGivenProfile $outcomeProbability */
+                $userProbability = new UserProbability();
+                $userProbability->setUser($user);
+                $userProbability->setLearningEntity($agora);
+                $userProbability->setProfile($outcomeProbability->getProfile());
+                $userProbability->setProbability(0.2);
+
+                $this->entityManager->persist($userProbability);
+                $userProbabilities[] = $userProbability;
+            }
+            $this->entityManager->flush();
+        }
+
+        $denominator = 0;
+        foreach ($userProbabilities as $userProbability) {
+            /* @var UserProbability $userProbability */
+            foreach ($outcomeProbabilities as $outcomeProbability) {
+                /* @var ProbabilityGivenProfile $outcomeProbability */
+                if ($outcomeProbability->getProfile()->getId() == $userProbability->getProfile()->getId()) {
+                    $denominator+= $outcomeProbability->getProbability() * $userProbability->getProbability();
+                }
+            }
+        }
+        $maxProbability = 0;
+        foreach ($userProbabilities as $userProbability) {
+            /* @var UserProbability $userProbability */
+            foreach ($outcomeProbabilities as $outcomeProbability) {
+                /* @var ProbabilityGivenProfile $outcomeProbability */
+                if ($outcomeProbability->getProfile()->getId() == $userProbability->getProfile()->getId()) {
+                    $userProbability->setProbability($userProbability->getProbability() * $outcomeProbability->getProbability() / $denominator);
+                    $maxProbability = max($maxProbability,$userProbability->getProbability());
+                    $this->entityManager->persist($userProbability);
+                }
+            }
+        }
+
+        /* @var Affinity $affinity */
+        $affinity = $this->entityManager->getRepository('LaCoreBundle:Affinity')->findOneBy(array('user'=>$user,'agora'=>$agora));
+        $affinity->setValue(100*$maxProbability);
+        $this->entityManager->persist($affinity);
+        $this->entityManager->flush();
     }
 }
