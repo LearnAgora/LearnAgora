ea7fa4330f3c13d5ecb53806c1115e29536c1851
Bart Kindt <bart.kindt@gmail.com>
2015-03-17 08:41:29 +0100

    Move the personacomparator code to its final destination

--- src/La/CoreBundle/Event/Listener/PersonaComparator.php
+++ src/La/CoreBundle/Event/Listener/PersonaComparator.php
@@ -20,4 +20,32 @@ class PersonaComparator
     {
         //        $this->compareWithPersona($user);
     }
+
+    /*
+    private function compareWithPersona($user)
+    {
+        $personalities = $this->personaRepository->findAll();
+
+        $comparePersona = new ComparePersona();
+
+        foreach ($personalities as $personality) {
+            $difference = $comparePersona->compare($user,$personality->getUser());
+            $personaMatch = $this->personaMatchRepository->findOneBy(
+                array(
+                    'user' => $user,
+                    'persona' => $personality
+                )
+            );
+            if (!$personaMatch) {
+                $personaMatch = new PersonaMatch();
+                $personaMatch->setUser($user);
+                $personaMatch->setPersona($personality);
+            }
+            $personaMatch->setDifference($difference);
+            $this->entityManager->persist($personaMatch);
+        }
+        $this->entityManager->flush();
+    }
+    */
+
 }
