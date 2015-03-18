c939b4a26f0ec147a94d21a081ab8829d885264b
Bart Kindt <bart.kindt@gmail.com>
2015-03-18 10:20:00 +0100

    LN-274 : some update on doctrine

--- src/La/CoreBundle/Entity/Outcome.php
+++ src/La/CoreBundle/Entity/Outcome.php
@@ -3,6 +3,7 @@
 namespace La\CoreBundle\Entity;
 
 use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
 use La\CoreBundle\Visitor\VisitorInterface;
@@ -50,7 +51,7 @@ abstract class Outcome
     }
 
     /**
-     * @var \La\CoreBundle\Entity\LearningEntity
+     * @var LearningEntity
      */
     private $learningEntity;
 
@@ -58,10 +59,10 @@ abstract class Outcome
     /**
      * Set learningEntity
      *
-     * @param \La\CoreBundle\Entity\LearningEntity $learningEntity
+     * @param LearningEntity $learningEntity
      * @return Outcome
      */
-    public function setLearningEntity(\La\CoreBundle\Entity\LearningEntity $learningEntity = null)
+    public function setLearningEntity(LearningEntity $learningEntity = null)
     {
         $this->learningEntity = $learningEntity;
 
@@ -71,7 +72,7 @@ abstract class Outcome
     /**
      * Get learningEntity
      *
-     * @return \La\CoreBundle\Entity\LearningEntity
+     * @return LearningEntity
      */
     public function getLearningEntity()
     {
@@ -82,7 +83,7 @@ abstract class Outcome
 
 
     /**
-     * @var \Doctrine\Common\Collections\Collection
+     * @var Collection
      */
     private $results;
 
@@ -91,11 +92,11 @@ abstract class Outcome
      */
     public function __construct()
     {
-        $this->results = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->results = new ArrayCollection();
     }
 
     /**
-     * @var \Doctrine\Common\Collections\Collection
+     * @var Collection
      */
     private $traces;
 
@@ -103,10 +104,10 @@ abstract class Outcome
     /**
      * Add traces
      *
-     * @param \La\CoreBundle\Entity\Trace $traces
+     * @param Trace $traces
      * @return Outcome
      */
-    public function addTrace(\La\CoreBundle\Entity\Trace $traces)
+    public function addTrace(Trace $traces)
     {
         $this->traces[] = $traces;
 
@@ -116,9 +117,9 @@ abstract class Outcome
     /**
      * Remove traces
      *
-     * @param \La\CoreBundle\Entity\Trace $traces
+     * @param Trace $traces
      */
-    public function removeTrace(\La\CoreBundle\Entity\Trace $traces)
+    public function removeTrace(Trace $traces)
     {
         $this->traces->removeElement($traces);
     }
@@ -126,7 +127,7 @@ abstract class Outcome
     /**
      * Get traces
      *
-     * @return \Doctrine\Common\Collections\Collection
+     * @return Collection
      */
     public function getTraces()
     {
@@ -182,10 +183,10 @@ abstract class Outcome
     /**
      * Add probabilities
      *
-     * @param \La\CoreBundle\Entity\ProbabilityGivenProfile $probabilities
+     * @param OutcomeProbability $probabilities
      * @return Outcome
      */
-    public function addProbability(\La\CoreBundle\Entity\ProbabilityGivenProfile $probabilities)
+    public function addProbability(OutcomeProbability $probabilities)
     {
         $this->probabilities[] = $probabilities;
 
@@ -195,9 +196,9 @@ abstract class Outcome
     /**
      * Remove probabilities
      *
-     * @param \La\CoreBundle\Entity\ProbabilityGivenProfile $probabilities
+     * @param OutcomeProbability $probabilities
      */
-    public function removeProbability(\La\CoreBundle\Entity\ProbabilityGivenProfile $probabilities)
+    public function removeProbability(OutcomeProbability $probabilities)
     {
         $this->probabilities->removeElement($probabilities);
     }
@@ -205,7 +206,7 @@ abstract class Outcome
     /**
      * Get probabilities
      *
-     * @return \Doctrine\Common\Collections\Collection 
+     * @return Collection
      */
     public function getProbabilities()
     {
--- src/La/CoreBundle/Entity/Profile.php
+++ src/La/CoreBundle/Entity/Profile.php
@@ -3,6 +3,8 @@
 namespace La\CoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Profile
@@ -13,11 +15,21 @@ class Profile
      * @var integer
      */
     private $id;
+
     /**
      * @var string
      */
     private $name;
 
+    /**
+     * @var Collection
+     */
+    private $outcomeProbabilities;
+
+    /**
+     * @var Collection
+     */
+    private $userProbabilities;
 
     /**
      * Get id
@@ -50,5 +62,79 @@ class Profile
     public function getName()
     {
         return $this->name;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->outcomeProbabilities = new ArrayCollection();
+        $this->userProbabilities = new ArrayCollection();
+    }
+
+    /**
+     * Add outcomeProbabilities
+     *
+     * @param OutcomeProbability $outcomeProbabilities
+     * @return Profile
+     */
+    public function addOutcomeProbability(OutcomeProbability $outcomeProbabilities)
+    {
+        $this->outcomeProbabilities[] = $outcomeProbabilities;
+
+        return $this;
+    }
+
+    /**
+     * Remove outcomeProbabilities
+     *
+     * @param OutcomeProbability $outcomeProbabilities
+     */
+    public function removeOutcomeProbability(OutcomeProbability $outcomeProbabilities)
+    {
+        $this->outcomeProbabilities->removeElement($outcomeProbabilities);
+    }
+
+    /**
+     * Get outcomeProbabilities
+     *
+     * @return Collection
+     */
+    public function getOutcomeProbabilities()
+    {
+        return $this->outcomeProbabilities;
+    }
+
+    /**
+     * Add userProbabilities
+     *
+     * @param UserProbability $userProbabilities
+     * @return Profile
+     */
+    public function addUserProbability(UserProbability $userProbabilities)
+    {
+        $this->userProbabilities[] = $userProbabilities;
+
+        return $this;
+    }
+
+    /**
+     * Remove userProbabilities
+     *
+     * @param UserProbability $userProbabilities
+     */
+    public function removeUserProbability(UserProbability $userProbabilities)
+    {
+        $this->userProbabilities->removeElement($userProbabilities);
+    }
+
+    /**
+     * Get userProbabilities
+     *
+     * @return Collection
+     */
+    public function getUserProbabilities()
+    {
+        return $this->userProbabilities;
     }
 }
