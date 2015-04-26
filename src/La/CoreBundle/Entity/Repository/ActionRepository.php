cb82d11b79f2f55186fdd6c070782687d23a8641
Bart Kindt <bart.kindt@gmail.com>
2015-04-26 23:21:46 +0200

    LN-292 : select random card in case a techne goal is active

--- src/La/CoreBundle/Entity/Repository/ActionRepository.php
+++ src/La/CoreBundle/Entity/Repository/ActionRepository.php
@@ -80,8 +80,12 @@ class ActionRepository extends EntityRepository
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult('id', 'id');
 
-//        $sql = "SELECT a.id FROM (SELECT le.id as id FROM LearningEntity le, Uplink u WHERE le.discr='action' and le.id=u.child_id and u.parent_id=?) a left outer join (SELECT o.learning_entity_id as leid, t.user_id as uid FROM Outcome o, Trace t WHERE o.id=t.outcome_id AND t.user_id=? group by o.learning_entity_id) b on a.id=b.leid  WHERE b.uid IS NULL ORDER BY RAND() LIMIT 1";
-        $sql = "SELECT * from (select le.id as id, (select count(*)  from Outcome o, Trace t where t.user_id=? and t.outcome_id=o.id and o.learning_entity_id=le.id) as cnt from LearningEntity le, Uplink u where le.discr='action' and u.child_id=le.id and u.parent_id=? ) res where cnt=0 order by rand() limit 1";
+        if (is_a($agora,'La\CoreBundle\Entity\Agora')) {
+            $sql = "SELECT * from (select le.id as id, (select count(*)  from Outcome o, Trace t where t.user_id=? and t.outcome_id=o.id and o.learning_entity_id=le.id) as cnt from LearningEntity le, Uplink u where le.discr='action' and u.child_id=le.id and u.parent_id=? ) res where cnt=0 order by rand() limit 1";
+        }
+        if (is_a($agora,'La\CoreBundle\Entity\Techne')) {
+            $sql = "SELECT * from (select le.id as id, (select count(*)  from Outcome o, Trace t where t.user_id=? and t.outcome_id=o.id and o.learning_entity_id=le.id) as cnt from LearningEntity le, Uplink u, Uplink v where le.discr='action' and u.child_id=le.id and u.parent_id=v.child_id and v.parent_id=? ) res where cnt=0 order by rand() limit 1";
+        }
         $query = $this->getEntityManager()->createNativeQuery($sql, $rsm);
         $query->setParameter(1, $user->getId());
         $query->setParameter(2, $agora->getId());
@@ -96,8 +100,12 @@ class ActionRepository extends EntityRepository
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult('id', 'id');
 
-//        $sql = "SELECT a.id FROM (SELECT le.id as id FROM LearningEntity le, Uplink u WHERE le.discr='action' and le.id=u.child_id and u.parent_id=?) a left outer join (SELECT o.learning_entity_id as leid, t.user_id as uid FROM Outcome o, Trace t WHERE o.id=t.outcome_id AND t.user_id=? group by o.learning_entity_id) b on a.id=b.leid  WHERE b.uid IS NULL ORDER BY RAND() LIMIT 1";
-        $sql = "select * from (select le.id as id, (select o.caption from Outcome o, Trace t where t.user_id=? and t.outcome_id=o.id and o.learning_entity_id=le.id order by t.created_time desc limit 1) as caption from LearningEntity le, Uplink u where le.discr='action' and u.child_id=le.id and u.parent_id=?) res where res.caption='LATER' order by rand() limit 1";
+        if (is_a($agora,'La\CoreBundle\Entity\Agora')) {
+            $sql = "select * from (select le.id as id, (select o.caption from Outcome o, Trace t where t.user_id=? and t.outcome_id=o.id and o.learning_entity_id=le.id order by t.created_time desc limit 1) as caption from LearningEntity le, Uplink u where le.discr='action' and u.child_id=le.id and u.parent_id=?) res where res.caption='LATER' order by rand() limit 1";
+        }
+        if (is_a($agora,'La\CoreBundle\Entity\Techne')) {
+            $sql = "select * from (select le.id as id, (select o.caption from Outcome o, Trace t where t.user_id=? and t.outcome_id=o.id and o.learning_entity_id=le.id order by t.created_time desc limit 1) as caption from LearningEntity le, Uplink u, Uplink v where le.discr='action' and u.child_id=le.id and u.parent_id=v.child_id and v.parent_id=?) res where res.caption='LATER' order by rand() limit 1";
+        }
         $query = $this->getEntityManager()->createNativeQuery($sql, $rsm);
         $query->setParameter(1, $user->getId());
         $query->setParameter(2, $agora->getId());
