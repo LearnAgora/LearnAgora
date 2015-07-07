0500211b1000b7c10ab76a7138ec16a74b7b09d7
Bart Kindt <bart.kindt@gmail.com>
2015-07-08 00:34:31 +0200

    LN-333: direct entry via not yet meaningful URL's

--- src/La/LearnodexBundle/Controller/Api/CardController.php
+++ src/La/LearnodexBundle/Controller/Api/CardController.php
@@ -2,8 +2,11 @@
 
 namespace La\LearnodexBundle\Controller\Api;
 
+use Doctrine\Common\Persistence\ObjectRepository;
 use FOS\RestBundle\View\View;
 use JMS\DiExtraBundle\Annotation as DI;
+use La\CoreBundle\Entity\LearningEntity;
+use La\LearnodexBundle\Model\Card;
 use La\LearnodexBundle\Model\Exception\CardNotFoundException;
 use La\LearnodexBundle\Model\RandomCardProviderInterface;
 use Nelmio\ApiDocBundle\Annotation as Doc;
@@ -17,17 +20,25 @@ class CardController
     private $randomCardProvider;
 
     /**
+     * @var ObjectRepository
+     */
+    private $learningEntityRepository;
+
+    /**
      * Constructor.
      *
      * @param RandomCardProviderInterface $randomCardProvider
+     * @param ObjectRepository $learningEntityRepository
      *
      * @DI\InjectParams({
-     *     "randomCardProvider" = @DI\Inject("random_card_provider")
+     *     "randomCardProvider" = @DI\Inject("random_card_provider"),
+     *     "learningEntityRepository" = @DI\Inject("la_core.repository.learning_entity")
      * })
      */
-    public function __construct(RandomCardProviderInterface $randomCardProvider)
+    public function __construct(RandomCardProviderInterface $randomCardProvider, ObjectRepository $learningEntityRepository)
     {
         $this->randomCardProvider = $randomCardProvider;
+        $this->learningEntityRepository = $learningEntityRepository;
     }
 
     /**
@@ -51,6 +62,17 @@ class CardController
             throw new NotFoundHttpException('No random card could be found.', $e);
         }
 
+        return View::create($card, 200);
+    }
+
+    public function cardAction($cardId)
+    {
+        $learningEntity = $this->learningEntityRepository->find($cardId);
+        if (!$learningEntity)
+            throw new NotFoundHttpException('Card could not be found.');
+
+        /** @var LearningEntity $learningEntity */
+        $card = new Card($learningEntity);
         return View::create($card, 200);
     }
 }
