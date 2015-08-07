c02c0e094b59ee8659ccf0022d2905b1d2943f14
Bart Kindt <bart.kindt@gmail.com>
2015-08-07 11:39:01 +0200

    stop sending downlinks embedded in the learningEntity

--- src/La/CoreBundle/Entity/Techne.php
+++ src/La/CoreBundle/Entity/Techne.php
@@ -3,20 +3,9 @@
 namespace La\CoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Hateoas\Configuration\Annotation as Hateoas;
-use JMS\Serializer\Annotation as Serializer;
 use La\CoreBundle\Visitor\TechneVisitorInterface;
 use La\CoreBundle\Visitor\VisitorInterface;
 
-/**
- * @Serializer\ExclusionPolicy("all")
- *
- * @Hateoas\Relation(
- *     "downlinks",
- *     embedded = "expr(object.getDownlinks())",
- *     exclusion = @Hateoas\Exclusion(excludeIf = "expr(object.getDownlinks() === null)")
- * )
- */
 class Techne extends AgoraBase
 {
     public function accept(VisitorInterface $visitor)
