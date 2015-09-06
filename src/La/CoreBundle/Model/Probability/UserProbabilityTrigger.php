7af9b80060a6fb785586b55e0f3e0ccfbed96d5f
Bart Kindt <bart.kindt@gmail.com>
2015-09-06 18:59:24 +0200

    limit the notifications to fluent and techne

--- src/La/CoreBundle/Model/Probability/UserProbabilityTrigger.php
+++ src/La/CoreBundle/Model/Probability/UserProbabilityTrigger.php
@@ -26,28 +26,32 @@ class UserProbabilityTrigger
         $this->events = array();
         foreach ($userProbabilities as $userProbability) {
             /** @var UserProbability $userProbability */
-            $probability = $userProbability->getProbability();
-            $eventOn90 = $probability > 0.9;
-            $eventOn50 = $probability > 0.5 && !$eventOn90;
-            if ($eventOn50 || $eventOn90) {
-                foreach ($userProbability->getEvents() as $event) {
-                    /** @var UserProbabilityEvent $event */
-                    $eventOn50 = $eventOn50 && $event->getTreshold() != 50;
-                    $eventOn90 = $eventOn90 && $event->getTreshold() != 90;
+            if ($userProbability->getProfile()->getName() == "Fluent") {
+                $probability = $userProbability->getProbability();
+                $learningEntity = $userProbability->getLearningEntity();
+                $eventOn90 = $probability > 0.9;
+                $eventOn50 = $probability > 0.5 && !$eventOn90 && is_a($learningEntity,'La\CoreBundle\Entity\Techne') ;
+                if ($eventOn50 || $eventOn90) {
+                    foreach ($userProbability->getEvents() as $event) {
+                        /** @var UserProbabilityEvent $event */
+                        $eventOn50 = $eventOn50 && $event->getTreshold() != 50;
+                        $eventOn90 = $eventOn90 && $event->getTreshold() != 90;
+                    }
+                }
+                if ($eventOn50 || $eventOn90) {
+                    $event = new UserProbabilityEvent();
+                    $event->setUserProbability($userProbability);
+                    $event->setMessage("well done");
+                    if ($eventOn50) {
+                        $event->setTreshold(50);
+                    } else {
+                        $event->setTreshold(90);
+                    }
+                    $event->setCreatedOn(new DateTime(date('Y-m-d H:i:s', time())));
+                    $this->events[] = $event;
                 }
             }
-            if ($eventOn50 || $eventOn90) {
-                $event = new UserProbabilityEvent();
-                $event->setUserProbability($userProbability);
-                $event->setMessage("well done");
-                if ($eventOn50) {
-                    $event->setTreshold(50);
-                } else {
-                    $event->setTreshold(90);
-                }
-                $event->setCreatedOn(new DateTime(date('Y-m-d H:i:s', time())));
-                $this->events[] = $event;
-            }
+
         }
         return $this->events;
     }
