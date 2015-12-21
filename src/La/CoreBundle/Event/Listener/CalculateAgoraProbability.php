900ee16f519ed11334d063be41baed69074a008e
Bart Kindt <bart.kindt@gmail.com>
2015-12-21 22:24:04 +0100

    restore events for skills (>90%)

--- src/La/CoreBundle/Event/Listener/CalculateAgoraProbability.php
+++ src/La/CoreBundle/Event/Listener/CalculateAgoraProbability.php
@@ -167,25 +167,21 @@ class CalculateAgoraProbability
 
             $this->bayesTheorem->applyTo($this->userProbabilityCollection, $this->outcomeProbabilityCollection);
 
-            /*
             $events = $this->userProbabilityTrigger->getEvents($userProbabilities);
             foreach ($events as $event) {
                 $this->entityManager->persist($event);
                 $user->addEvent($event);
             }
-            */
             $this->entityManager->flush();
 
             $this->eventDispatcher->dispatch(Events::USER_PROBABILITY_UPDATED, new UserProbabilityUpdatedEvent($user,$agora));
         }
 
-        /*
         $events = $this->userTraceTrigger->getEvents($user);
         foreach ($events as $event) {
             $this->entityManager->persist($event);
             $user->addEvent($event);
         }
-        */
         $this->entityManager->flush();
     }
 }
